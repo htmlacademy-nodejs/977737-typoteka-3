084b442787a274cc37109c80b542bb92e0df0102
pavel <kovrizhkopavel@gmail.com>
2022-04-29 19:50:16 +0300

    feat: make module3-task2

--- src/express/express.js
+++ src/express/express.js
@@ -4,13 +4,22 @@ const express = require(`express`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const userRoutes = require(`./routes/user-routes`);
 const mainRoutes = require(`./routes/main-routes`);
+const path = require(`path`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
 
 app.use(`/articles`, articlesRoutes);
 app.use(`/user`, userRoutes);
 app.use(`/`, mainRoutes);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use((req, res) => res.status(400).render(`errors/404`));
+app.use((err, req, res) => res.status(500).render(`errors/500`));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(DEFAULT_PORT);
