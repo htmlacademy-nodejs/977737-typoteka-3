7765aaf721b5d43109a7589911bac8023a312292
pavel <kovrizhkopavel@gmail.com>
2022-05-01 21:33:03 +0300

    feat: make module3-task3

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,61 +1,25 @@
 'use strict';
 
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
 const {HttpCode, customConsole} = require(`../../utils`);
 
 const DEFAULT_PORT = 3000;
 const MOCK_FILE = `mocks.json`;
 
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(MOCK_FILE);
-        const mocks = JSON.parse(fileContent);
-        const title = mocks.map((article) => `<li>${article.title}</li>`).join(``);
-        sendResponse({
-          res,
-          statusCode: HttpCode.OK,
-          content: `<ul>${title}</ul>`
-        });
-      } catch (err) {
-        sendResponse({
-          res,
-          statusCode: HttpCode.NOT_FOUND,
-          content: notFoundMessageText
-        });
-      }
-      break;
-    default:
-      sendResponse({
-        res,
-        statusCode: HttpCode.NOT_FOUND,
-        content: notFoundMessageText,
-      });
-      break;
+const app = express();
+app.use(express.json());
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(MOCK_FILE);
+    const mockData = JSON.parse(fileContent) || [];
+    res.json(mockData);
+  } catch (_err) {
+    res.json([]);
   }
-};
+});
 
-const sendResponse = (params) => {
-  const {res, statusCode, content} = params;
-  const template = `
-    <!Doctype html>
-    <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${content}</body>
-    </html>`.trim();
-
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template);
-};
+app.use((req, res) => res.status(HttpCode.NOT_FOUND).send(`Not found3`));
 
 module.exports = {
   name: `--server`,
@@ -63,14 +27,13 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, () => {
-        customConsole.info(`Ожидаю соединений на ${port}`);
-      })
-      .on(`error`, ({message}) => {
-        customConsole.error(`Ошибка при создании сервера: ${message}`);
-      });
+    app.listen(port, (err) => {
+      if (err) {
+        return customConsole.error(`Ошибка при создании сервера`, err);
+      }
+
+      return customConsole.info(`Ожидаю соединений на ${port}`);
+    });
   }
 };
 
