c6373aa7c144f58e1a7ec54480538448a5fc2e6f
pavel <kovrizhkopavel@gmail.com>
2022-06-13 23:23:45 +0300

    feat: make module5-task1

---
+++ schema.sql
@@ -0,0 +1,43 @@
+CREATE TABLE categories(
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(255) NOT NULL
+);
+
+CREATE TABLE users(
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(255) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
+    password_hash varchar(255) NOT NULL,
+    avatar varchar(50) NOT NULL
+);
+
+CREATE TABLE articles(
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    photo_file varchar(50) NOT NULL,
+    created_at timestamp NOT NULL,
+    announcement varchar(255) NOT NULL,
+    title varchar(255) NOT NULL,
+    text text NOT NULL,
+    user_id integer NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+    id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    article_id integer NOT NULL,
+    user_id integer NOT NULL,
+    text text NOT NULL,
+    created_at timestamp NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (article_id) REFERENCES articles(id)
+);
+
+CREATE TABLE aricle_categories(
+    article_id integer NOT NULL,
+    category_id integer NOT NULL,
+    PRIMARY KEY (article_id, category_id),
+    FOREIGN KEY (article_id) REFERENCES articles(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE INDEX ON articles(title);
