5d7868e90364a91ddb454d8ffd47e06603c46b35
pavel <kovrizhkopavel@gmail.com>
2022-06-15 01:31:44 +0300

    feat: make module5-task2

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -4,12 +4,14 @@ const help = require(`./help`);
 const generate = require(`./generate`);
 const version = require(`./version`);
 const server = require(`./server`);
+const fill = require(`./fill`);
 
 const Cli = {
   [help.name]: help,
   [generate.name]: generate,
   [version.name]: version,
   [server.name]: server,
+  [fill.name]: fill,
 };
 
 module.exports = {
