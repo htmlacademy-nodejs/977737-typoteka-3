143e8f454d71701328f08f800f4c3298a800debb
pavel <kovrizhkopavel@gmail.com>
2022-06-29 22:40:37 +0300

    feat: make models

---
+++ src/service/models/article.js
@@ -0,0 +1,30 @@
+/* eslint-disable new-cap */
+'use strict';
+
+const {DataTypes, Model} = require(`sequelize`);
+
+const MaxLength = {
+  TEXT: 1000,
+  ANNOUNCEMENT: 100,
+};
+
+class Article extends Model {}
+
+const define = (sequelize) => Article.init({
+  photoFile: DataTypes.STRING,
+  announcement: {
+    type: DataTypes.STRING(MaxLength.ANNOUNCEMENT),
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: null,
+  },
+  text: DataTypes.STRING(MaxLength.TEXT),
+}, {
+  sequelize,
+  modelName: `Article`,
+  tableName: `articles`,
+});
+
+module.exports = define;
